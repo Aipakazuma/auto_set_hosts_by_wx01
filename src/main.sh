ddade656c4d1e22a9819f72c552836cc0dbd09e4
aipacommander <gundamgundam777@gmail.com>
2015-12-03 10:45:51 +0900

    [add] add timeout value
    また、表示をアクセスできるIPアドレスのみに設定

--- src/main.sh
+++ src/main.sh
@@ -4,8 +4,7 @@ ROOP_NUM=13
 
 for i in `seq 1 ${ROOP_NUM}`:
 do
-  echo ${IP_ADDR}${i}
-  STATUS_CODE=`curl -LI http://${IP_ADDR}${i}/active.txt -o /dev/null -w '%{http_code}\n' -s`
+  STATUS_CODE=`curl -LI http://${IP_ADDR}${i}/active.txt -o /dev/null -w '%{http_code}\n' -m 1 -s`
   if [ "${STATUS_CODE}" = "200" ]; then
     echo ${IP_ADDR}${i}
     break
